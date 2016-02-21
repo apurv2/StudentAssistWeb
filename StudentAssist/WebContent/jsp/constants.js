e5809060833997eb6defe6b71d03b6c04f4c82c0
apurv2 <apurv2@gmail.com>
2016-02-21 13:33:10 -0500

    bug fixes

--- StudentAssist/WebContent/jsp/constants.js
+++ StudentAssist/WebContent/jsp/constants.js
@@ -1,57 +1,40 @@
-noOfRooms = [
-	                        '1 bhk/1 bath',
-	                        '1 bhk/2 bath',
-	                        '2 bhk/1 bath',
-	                        '2 bhk/2 bath',
-	                        '3 bhk/1 bath',
-	                        '3 bhk/2 bath',
-	                        '3 bhk/3 bath',
-	                        'Shared',
-	                        'Other',
-	                      ];
-	
-	vacancies = [
-	                        '1',
-	                        '2',
-	                        '3',
-	                        '4',
-	                        'Other',
-	                        'Lease Transfer',
-	                      ];
-	
-	gender = [
-	                        'Male',
-	                        'Female',
-	                         'Doesnt Matter'
-	                      ];
-	
-	
-	apartmenTypes = [
-	                        'On-Campus',
-	                        'Off-Campus',
-	                        'Dorms'
-	                      ];
-	         
-	
-	
-	var map={};
-	map["On-Campus"]="on";
-	map["Off-Campus"]="off";
-	map["Dorms"]="dorms";
-	
-	var url="http://studentassist.elasticbeanstalk.com/rest/accommodation/";
+noOfRooms = [ '1 bhk/1 bath', '1 bhk/2 bath', '2 bhk/1 bath', '2 bhk/2 bath',
+		'3 bhk/1 bath', '3 bhk/2 bath', '3 bhk/3 bath', 'Shared', 'Other', ];
 
-	
-	var APARTMENT_NAME="apartmentName";
-	var NO_OF_ROOMS="noOfRooms";
-	var VACANCIES="vacancies";
-	var COST="cost";
-	var GENDER="gender";
-	var FBID="fbId";
-	var NOTES="notes";
-	var USER_ID="userId";
-	var POST_ACCOMMODATION="createAccommodationAddFromFacebook";
-	var ON_CAMPUS="On-Campus";
-	
-	
-	
+vacancies = [ '1', '2', '3', '4', 'Other', 'Lease Transfer'];
+
+gender = [ 'Male', 'Female', 'Doesnt Matter' ];
+
+apartmentTypes = [ 'On-Campus', 'Off-Campus', 'Dorms' ];
+
+leftSpinnerValues = [ 'Apartment Type', 'Apartment Name', 'Gender' ];
+
+
+
+var map = {};
+map["On-Campus"] = "on";
+map["Off-Campus"] = "off";
+map["Dorms"] = "dorms";
+
+var leftSpinnerCodeMap=[];
+
+leftSpinnerCodeMap["Apartment Type"]= "apartmentType";
+leftSpinnerCodeMap["Apartment Name"]= "apartmentName";
+leftSpinnerCodeMap["Gender"]= "gender";
+
+
+var url = "http://studentassist.elasticbeanstalk.com/rest/accommodation/";
+
+var APARTMENT_NAME = "apartmentName";
+var NO_OF_ROOMS = "noOfRooms";
+var VACANCIES = "vacancies";
+var COST = "cost";
+var GENDER = "gender";
+var FBID = "fbId";
+var NOTES = "notes";
+var USER_ID = "userId";
+var POST_ACCOMMODATION = "createAccommodationAddFromFacebook";
+var ON_CAMPUS = "On-Campus";
+var APARTMENT_TYPE='Apartment Type';
+var APARTMENT_NAME='Apartment Name';
+var GENDER_SPINNER='Gender';
--- StudentAssist/WebContent/jsp/example.js
+++ StudentAssist/WebContent/jsp/example.js
@@ -4,53 +4,38 @@ var module = angular.module('ui.bootstrap.demo');
 
 module.service('StudentAssist', function($http, $log) {
 
-	var advertisements = [];
-
-	var apartmentNames = [];
-
-	var apartmentNamesTitle =[ {
-		apartmentName : 'apartmentNames'
-	}]
-	
-	
-	//returns the apartment names Title
-	this.getApartmentsTitle = function() {
-
-		return apartmentNamesTitle;
-	}
-	
-	
-	//returns all apartmentNames list
-	this.getAllapartmentsList = function() {
-
-		return apartmentNames;
-	}
-
-	// returns all advertisements
-	this.getSimpleSearchAddsList = function() {
-
-		return advertisements;
-
-	}
-
-	this.getApartmentNames = function(apartmentType) {
+	/*this.getApartmentNames = function(leftSpinner) {
 
 		var aptNamesurl = url + "getApartmentNames?apartmentType="
-				+ map[apartmentType];
+				+ map[leftSpinner];
+		
+		$log.log(aptNamesurl);
 
-		$http.get(aptNamesurl).then(function(response) {
-
-			// push apartment names to populate drop-down 
-			apartmentNames.length = 0;
-			apartmentNames.push.apply(apartmentNames, response.data);
-			
-			//push apartment names to change the title of the drop-down
-			apartmentNamesTitle.length=0;
-			apartmentNamesTitle.push.apply(apartmentNamesTitle,response.data);
-
-		});
-
+		return $http.get(aptNamesurl);
+	};
+*/
+	
+	this.getAllApartmentNames = function()
+	{
+		var aptNamesurl=url+"getAllApartmentNames";
+		
+		
+		return $http.get(aptNamesurl);
 	}
+	
+	
+	this.getSimpleSearchAdds = function(leftSpinner, rightSpinner) {
+
+		
+		
+		var urlAcc = url + "getSimpleSearchAdds?leftSpinner=" + leftSpinner
+				+ "&rightSpinner=" + rightSpinner;
+
+		return $http.get(urlAcc);
+
+	};
+	
+	
 
 	this.submitAdd = function(url) {
 		$http.get(submitUrl).then(function(response) {
@@ -59,64 +44,130 @@ module.service('StudentAssist', function($http, $log) {
 
 	}
 
-	this.getSimpleSearchAdds = function(leftSpinner, rightSpinner) {
-		var urlAcc = url + "getSimpleSearchAdds?leftSpinner=" + leftSpinner
-				+ "&rightSpinner=" + rightSpinner;
-
-		$http.get(urlAcc).then(function(response) {
-
-			advertisements.length = 0;
-			advertisements.push.apply(advertisements, response.data);
-
-		});
-
-	}
+	
 
 });
 
 module.controller('DropdownCtrl', function($scope, $log, $http, StudentAssist) {
 
-	// get the apartment names list
-	$scope.apartmentNames = StudentAssist.getAllapartmentsList();
+
+
+	$scope.leftSpinnerHeader = APARTMENT_TYPE;
+	$scope.rightSpinnerHeader = "";
+
+	$scope.rightSpinnerValues = [];
+	$scope.advertisements=[];
 
 	$scope.default_no_rooms = "1 bhk/1 bath";
 	$scope.default_vacancies = "1";
 	$scope.default_gender = "Male";
-	$scope.apartmentNamesTitle = StudentAssist.getApartmentsTitle();
-
 	$scope.noOfRooms = noOfRooms;
 	$scope.vacancies = vacancies;
 	$scope.gender = gender;
 
-	// initilize apartmentTypes
-	$scope.apartmenTypes = apartmenTypes;
+	// initilize leftSpinner
+	$scope.leftSpinnerValues = leftSpinnerValues;
 
-	// initialize simple Search advertisements
-	$scope.advertisements = StudentAssist.getSimpleSearchAdds();
+	$scope.getSimpleSearchAds = function(leftSpinner,rightSpinner) {
+		
+		
+		var queryparam="";
+		
+		
+		queryparam=map[rightSpinner]==null?rightSpinner:map[rightSpinner];
+		
+		leftSpinner= leftSpinnerCodeMap[leftSpinner];
+		rightSpinner = encodeURIComponent(queryparam);
+		
+		$log.log(leftSpinner);
 
-	// populate default apartment names when application starts.
-	StudentAssist.getApartmentNames(ON_CAMPUS);
+		
+		StudentAssist.getSimpleSearchAdds(leftSpinner,rightSpinner).success(
+						function(response) {
+
+							$log.log(response);
+							
+							$scope.advertisements.length=0;	
+							$scope.advertisements.push.apply($scope.advertisements,response);
+							
+						
+						}).error(function(response) {
+				});
+
+	}
 	
-	// gets simple serarch adds
-	$scope.getAccommodationAdds = function(apartmentName) {
-		
-		var temp = [ {
-			apartmentName : apartmentName
-		} ];
 
-		$scope.apartmentNamesTitle.length = 0;
+	/*
+	 * 1. changes value of left spinner 2. gets new values for right spinner 3.
+	 * gets the advertisements related to left and right spinners.
+	 */
+	$scope.leftSpinner = function(leftSpinner) {
+		
+		$scope.leftSpinnerHeader = leftSpinner;
+		
+		if(leftSpinner==APARTMENT_TYPE)
+			{
+			
+			$scope.rightSpinnerValues = apartmentTypes;
+			$scope.rightSpinnerHeader = apartmentTypes[0];
 
-		$scope.apartmentNamesTitle.push.apply($scope.apartmentNamesTitle,
-				temp);
+			$scope.getSimpleSearchAds($scope.leftSpinnerHeader,$scope.rightSpinnerHeader);
+
+			}
+		else if(leftSpinner==APARTMENT_NAME)
+			{
+			StudentAssist.getAllApartmentNames().success(
+					function(response) {
+						
+						$scope.rightSpinnerValues.length=0;
+						
+						for(var i=0;i<response.length;i++)
+							{
+							
+							$scope.rightSpinnerValues.push(response[i].apartmentName);
+
+							
+							}
+						
+						
+						$scope.rightSpinnerHeader = response[0].apartmentName;
+						
+						$scope.getSimpleSearchAds($scope.leftSpinnerHeader,$scope.rightSpinnerHeader);
+					}).error(function(response) {
+			});
+			
+			}
+		else
+			{
+			$scope.rightSpinnerValues = gender;
+			$scope.rightSpinnerHeader = gender[0];
+			$scope.getSimpleSearchAds($scope.leftSpinnerHeader,$scope.rightSpinnerHeader);
+
+			}
+
 		
+
+	}
+
+	
+	// calling left spinner to get the deafult values at page load.
+	$scope.leftSpinner($scope.leftSpinnerHeader);
+
+	// 1. changes the value of right spinner.
+	// 2. ets the advertisements related to left and right spinners.
+	$scope.rightSpinner = function(rightSpinner) {
+
+		$log.log(rightSpinner);
 		
-		//StudentAssist.getSimpleSearchAdds();
+		$scope.rightSpinnerHeader = rightSpinner;
+		$scope.getSimpleSearchAds($scope.leftSpinnerHeader,$scope.rightSpinnerHeader);
+
 
 	}
 
 	$scope.submitClicked = function() {
 
-		var apartmentName = $scope.apartmentNames[0].apartmentName;
+		var apartmentName = $scope.rightSpinnerValues[0].apartmentName;
 
 		var parameters = POST_ACCOMMODATION + "?" + APARTMENT_NAME + "="
 				+ encodeURIComponent(apartmentName) + "&" + NO_OF_ROOMS + "="
@@ -134,23 +185,6 @@ module.controller('DropdownCtrl', function($scope, $log, $http, StudentAssist) {
 
 	}
 
-	
-	
-	$scope.changeAptName = function(apartmentName) {
-
-		var temp = [ {
-			apartmentName : apartmentName
-		} ];
-
-		$scope.apartmentNamesTitle.length = 0;
-
-		$scope.apartmentNamesTitle.push.apply($scope.apartmentNamesTitle,
-				temp);
-		
-		
-
-	}
-
 	$scope.changeVacancies = function(vacancies) {
 
 		$scope.default_vacancies = vacancies;
@@ -164,18 +198,6 @@ module.controller('DropdownCtrl', function($scope, $log, $http, StudentAssist) {
 	$scope.changeNoOfRooms = function(noOfRooms) {
 
 		$scope.default_no_rooms = noOfRooms;
-	}
-
-	$scope.changeApartmentType = function(apartmentType) {
-
-		$scope.apartmentType = apartmentType;
-
-		StudentAssist.getApartmentNames(apartmentType);
-
-	}
-
-	$scope.init = function() {
-		$scope.apartmentType = apartmenTypes[0];
 	}
 
 	$scope.status = {
